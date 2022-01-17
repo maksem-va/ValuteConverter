81bb26eb3523dea927cc1b144d7b2d399a216df5
maksem-va <senLu7zi>
2022-01-17 19:14:36 +0300

    First commit. First converter

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def cb_page_getter():
+    inp = input('Input request. ').split()
+    print(inp[0] + " " + inp[1].upper() + " equals " + str(float(inp[0]) * 30) + " RUB")
 
 
-# Press the green button in the gutter to run the script.
+# 30 рублей как в 2007
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cb_page_getter()
