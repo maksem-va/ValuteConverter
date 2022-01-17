4f5bad9f877728fe9effd9dd0546246bff924659
maksem-va <senLu7zi>
2022-01-17 19:18:47 +0300

    Second commit. Added CBR database

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+from xml.etree import ElementTree
+
+import requests
+
+
 def cb_page_getter():
-    inp = input('Input request. ').split()
-    print(inp[0] + " " + inp[1].upper() + " equals " + str(float(inp[0]) * 30) + " RUB")
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    inp = input('Input request. Format: Target Valute Number ').split()
+    page = requests.get(url)
+    tree = ElementTree.fromstring(page.content)
+    xmlDict = {}
+    for dt in tree.findall('Valute'):
+        code = dt.find('CharCode').text
+        vle = dt.find('Value').text
+        nom = dt.find('Nominal').text
+        name = dt.find('Name').text
+        xmlDict[code.lower()] = vle + " " + nom + " " + name
+    string = xmlDict[inp[0].lower()].split()
+    wage = string[0].replace(',', '.')
+    print(inp[1] + " " + inp[0].upper() + " equals " + str(float(inp[1]) * float(wage)) + " RUB")
 
-
-# 30 рублей как в 2007
 
 if __name__ == '__main__':
     cb_page_getter()
