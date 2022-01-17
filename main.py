a60d00cbb44d1fa8ab6b003a40f1eab1c579d83a
maksem-va <senLu7zi>
2022-01-17 19:30:08 +0300

    Fourth commit. -faq enabled

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 
 def cb_page_getter():
     url = 'http://www.cbr.ru/scripts/XML_daily.asp'
-    inp = input('Input request. Format: Target Valute Number ').split()
+    inp = input('Input request. "-faq" for additional information. ').split()
     page = requests.get(url)
     tree = ElementTree.fromstring(page.content)
     xmlDict = {}
@@ -17,11 +17,14 @@ def cb_page_getter():
         name = dt.find('Name').text
         xmlDict[code.lower()] = vle + " " + nom + " " + name
     if inp[0] == '-codes':
-        for cd in tree.findall('Valute'):
-            code = cd.find('CharCode').text
-            name = cd.find('Name').text
+        for cde in tree.findall('Valute'):
+            code = cde.find('CharCode').text
+            name = cde.find('Name').text
             codeDict[code] = name
         return print(codeDict)
+    if inp[0] == '-faq':
+        return print('Thank you for using my application. Hope, that you will never use this app again. Use command'
+              ' -codes for list of available valutes. Input format: Incoming valute Value. Example: USD 10')
     string = xmlDict[inp[0].lower()].split()
     wage = string[0].replace(',', '.')
     print(inp[1] + " " + inp[0].upper() + " equals " + str(float(inp[1]) * float(wage)) + " RUB")
